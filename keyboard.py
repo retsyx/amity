6dbb8596da5af5083480e2f0ab666760584c1ec4
retsyx <retsyx@gmail.com>
2024-10-25 13:07:25 -0700

    Revert "Play pause button should play/pause instead of select"
    This reverts commit c3fce2a0056a154618bf176c719eee4d54117049.

--- keyboard.py
+++ keyboard.py
@@ -38,7 +38,7 @@ config.default('keyboard.keymap', {
         e.KEY_SEARCH : Key.DISPLAY_INFO.value,
         e.KEY_HOMEPAGE : Key.ROOT_MENU.value,
         e.KEY_REWIND : Key.REWIND.value,
-        e.KEY_PLAYPAUSE : Key.PAUSE_PLAY.value,
+        e.KEY_PLAYPAUSE : Key.SELECT.value,
         e.KEY_FASTFORWARD : Key.FAST_FORWARD.value,
         e.KEY_PROGRAM : Key.GUIDE.value,
         e.KEY_0 : Key.NUMBER_0.value,
