c2c4a4827c8d7a5b4a5a7292a0f4e21f2925895c
retsyx <retsyx@gmail.com>
2026-02-13 11:35:38 -0800

    remote adapter fix

--- remote_adapter.py
+++ remote_adapter.py
@@ -1,3 +1,11 @@
+# Copyright 2024-2025.
+# This file is part of Amity.
+# Amity is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This code is derived from code written by Yanndroid (https://github.com/Yanndroid)
+
 import tools
 
 log = tools.logger(__name__)
