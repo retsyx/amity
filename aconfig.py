b3abc9db8b40a40c98896a3ac769c840fdbedb68
retsyx <retsyx@gmail.com>
2026-02-14 00:27:20 -0800

    aconfig.py type annotation

--- aconfig.py
+++ aconfig.py
@@ -1,4 +1,4 @@
-# Copyright 2024.
+# Copyright 2024-2025.
 # This file is part of Amity.
 # Amity is free software: you can redistribute it and/or modify it under the terms of the
 # GNU General Public License as published by the Free Software Foundation, either version 3 of the
@@ -9,16 +9,19 @@ import tools
 log = tools.logger(__name__)
 
 import asyncio, os
+from collections.abc import Callable
 
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
 from watchdog.observers import Observer
+from watchdog.observers.api import ObservedWatch, BaseObserver
 
 from config import Config
 
-class Watcher(object):
-    def __init__(self, filename, callback):
-        self.loop = None
-        self.observer = None
-        self.watch = None
+class Watcher(FileSystemEventHandler):
+    def __init__(self, filename: str, callback: Callable[[], None]) -> None:
+        self.loop: asyncio.AbstractEventLoop | None = None
+        self.observer: BaseObserver | None = None
+        self.watch: ObservedWatch | None = None
         self.full_path = filename
         path = os.path.dirname(self.full_path)
         if path == '':
@@ -27,7 +30,7 @@ class Watcher(object):
         self.filename = os.path.basename(self.full_path)
         self.callback = callback
 
-    def start(self):
+    def start(self) -> None:
         if self.loop is None:
             self.loop = asyncio.get_event_loop()
         if self.observer is None:
@@ -36,7 +39,7 @@ class Watcher(object):
             self.observer.start()
             self.watch = self.observer.schedule(self, self.dir_path, recursive=False)
 
-    def dispatch(self, event):
+    def dispatch(self, event: FileSystemEvent) -> None:
         if (not event.is_directory and
                 (event.src_path == self.full_path and
                  event.event_type in ('created', 'modified')) or
@@ -44,10 +47,11 @@ class Watcher(object):
                   event.dest_path == self.full_path) and
                  event.event_type in ('moved', )):
             log.info(f'Event {event}')
-            self.loop.call_soon_threadsafe(self.callback)
+            if self.loop is not None:
+                self.loop.call_soon_threadsafe(self.callback)
 
 class ConfigWatcher(Watcher):
-    def __init__(self, config, callback):
+    def __init__(self, config: Config, callback: Callable[[], None]) -> None:
         super().__init__(config.filename, callback)
 
 config = Config('var/config/config.yaml')
